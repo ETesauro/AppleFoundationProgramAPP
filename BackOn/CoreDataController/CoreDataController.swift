6208d5ffe129a8e79dab28dbb09f928e442e8145
Emmanuel Tesauro <emmanuel.tesauro@gmail.com>
2020-02-16 17:20:54 +0100

    Aggiunto metodo getLoggedUser() in CoreDataController che restituisce una tupla (String, UserInfo) dove String può essere "Nil" se l'utente non esiste oppure "OK" se l'utente esiste.

--- BackOn/CoreDataController/CoreDataController.swift
+++ BackOn/CoreDataController/CoreDataController.swift
@@ -13,7 +13,7 @@ import UIKit
 class CoreDataController {
     static let shared = CoreDataController()
     private var context: NSManagedObjectContext
-
+    
     
     init() {
         let application = UIApplication.shared.delegate as! AppDelegate
@@ -27,8 +27,8 @@ class CoreDataController {
         newUser.name = user.name
         newUser.surname = user.surname
         newUser.userID = user.userID
-
-
+        
+        
         do {
             try self.context.save()
         } catch let errore {
@@ -42,24 +42,24 @@ class CoreDataController {
     
     func checkUser() -> Bool{
         print("[CDC] Recupero l'utente dal context ")
-
+        
         let fetchRequest: NSFetchRequest<PUser> = PUser.fetchRequest()
-
+        
         do {
             let array = try self.context.fetch(fetchRequest)
-
+            
             guard array.count > 0 else {
                 print("[CDC] Non ci sono utenti")
                 return false
             }
-
+            
             for x in array {
                 let user = x
                 print("[CDC] Utente \(user.name!), \(user.userID!)")
             }
-
+            
             return true
-
+            
         } catch let errore {
             print("[CDC] Problema esecuzione FetchRequest")
             print("  Stampo l'errore: \n \(errore) \n")
@@ -67,5 +67,35 @@ class CoreDataController {
         }
     }
     
+    
+    func getLoggedUser() -> (String, UserInfo){
+        print("[CDC] Recupero l'utente dal context ")
+        
+        let fetchRequest: NSFetchRequest<PUser> = PUser.fetchRequest()
+        
+        do {
+            let array = try self.context.fetch(fetchRequest)
+            
+            guard array.count > 0 else {
+                print("[CDC] Non ci sono utenti")
+                return ("Nil", UserInfo(photo: "", name: "", surname: ""))
+            }
+            
+            for x in array {
+                let user = x
+                print("[CDC] Utente \(user.name!), \(user.userID!)")
+            }
+            
+            //            photo = "" va cambiato
+            let myUser = UserInfo(photo: "", name: array[0].name!, surname: array[0].surname!)
+            
+            return ("OK", myUser)
+            
+        } catch let errore {
+            print("[CDC] Problema esecuzione FetchRequest")
+            print("  Stampo l'errore: \n \(errore) \n")
+            return ("Nil", UserInfo(photo: "", name: "", surname: ""))
+        }
+    }
     
 }
