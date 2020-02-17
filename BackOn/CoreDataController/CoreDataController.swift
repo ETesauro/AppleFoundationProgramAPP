feb05b068ab84569e1c8f629d0a38004b094a011
Vincenzo Riccio <viriccio@gmail.com>
2020-02-17 09:45:35 +0100

    Dettagli coredata e login

--- BackOn/CoreDataController/CoreDataController.swift
+++ BackOn/CoreDataController/CoreDataController.swift
@@ -14,86 +14,56 @@ class CoreDataController {
     static let shared = CoreDataController()
     private var context: NSManagedObjectContext
     
-    
     init() {
         let application = UIApplication.shared.delegate as! AppDelegate
         self.context = application.persistentContainer.viewContext
     }
     
-    func addUser(user: UserInfo) {
+    func addUser(user: UserInfo) { // save to CoreData
         let entity = NSEntityDescription.entity(forEntityName: "PUser", in: self.context)
-        
         let newUser = PUser(entity: entity!, insertInto: self.context)
         newUser.name = user.name
         newUser.surname = user.surname
         newUser.userID = user.userID
         
-        
         do {
             try self.context.save()
         } catch let errore {
-            print("[CDC] Problema salvataggio Libro: \(newUser.name!) in memoria")
+            print("Error in saving: \(newUser.name!) in memoria")
             print("  Stampo l'errore: \n \(errore) \n")
         }
         print("\(newUser.name!) salvato in memoria.")
         
     }
     
-    
-    func checkUser() -> Bool{
-        print("[CDC] Recupero l'utente dal context ")
-        
+    func userIsLogged() -> Bool{
         let fetchRequest: NSFetchRequest<PUser> = PUser.fetchRequest()
-        
         do {
             let array = try self.context.fetch(fetchRequest)
-            
             guard array.count > 0 else {
-                print("[CDC] Non ci sono utenti")
+                print("User not logged yet")
                 return false
             }
-            
-            for x in array {
-                let user = x
-                print("[CDC] Utente \(user.name!), \(user.userID!)")
-            }
-            
             return true
-            
-        } catch let errore {
-            print("[CDC] Problema esecuzione FetchRequest")
-            print("  Stampo l'errore: \n \(errore) \n")
+        } catch let error {
+            print("PUser fetchRequest error: \(error.localizedDescription)")
             return false
         }
     }
     
-    
     func getLoggedUser() -> (String, UserInfo){
-        print("[CDC] Recupero l'utente dal context ")
-        
         let fetchRequest: NSFetchRequest<PUser> = PUser.fetchRequest()
-        
         do {
             let array = try self.context.fetch(fetchRequest)
-            
             guard array.count > 0 else {
-                print("[CDC] Non ci sono utenti")
+                print("User not logged yet")
                 return ("Nil", UserInfo(photo: "", name: "", surname: ""))
             }
-            
-            for x in array {
-                let user = x
-                print("[CDC] Utente \(user.name!), \(user.userID!)")
-            }
-            
             //            photo = "" va cambiato
             let myUser = UserInfo(photo: "", name: array[0].name!, surname: array[0].surname!)
-            
             return ("OK", myUser)
-            
-        } catch let errore {
-            print("[CDC] Problema esecuzione FetchRequest")
-            print("  Stampo l'errore: \n \(errore) \n")
+        } catch let error {
+            print("Error while getting logged user: \(error.localizedDescription)")
             return ("Nil", UserInfo(photo: "", name: "", surname: ""))
         }
     }
